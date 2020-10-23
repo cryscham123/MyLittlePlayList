fc945791ad9f1c837953fc491ce89e59c4079a4a
cryscham123 <58943980+cryscham123@users.noreply.github.com>
2020-10-23 21:49:50 +0900

    update(20/10/23)2
    ^^

--- test.js
+++ test.js
@@ -1,10 +1,34 @@
 "use strict";
 
-const likes = document.querySelector(".likesBtn p");
-const playingsongBtn = document.querySelector(".likesBtn");
-let i = 0;
+const secOne = document.querySelector(".sec__one");
+const secTwo = document.querySelector(".sec__two");
+const secThree = document.querySelector(".sec__three");
+const secFour = document.querySelector(".sec__four");
+const secFive = document.querySelector(".sec__five");
 
-playingsongBtn.addEventListener("click", () => {
-  i += 1;
-  likes.innerHTML = `${i}`;
-});
+const home = document.querySelector(".HomeBtn");
+const team= document.querySelector(".TeamBtn");
+const menu= document.querySelector(".MenuBtn");
+const contact= document.querySelector(".ContactBtn");
+const user = document.querySelector(".UserBtn");
+
+home.addEventListener('click', () => {
+  secOne.scrollIntoView({behavior:"smooth", block:"center"});
+})
+team.addEventListener('click', () => {
+  secTwo.scrollIntoView({behavior:"smooth", block:"center"});
+})
+menu.addEventListener('click', () => {
+  secThree.scrollIntoView({behavior:"smooth", block:"center"});
+})
+contact.addEventListener('click', () => {
+  secFour.scrollIntoView({behavior:"smooth", block:"center"});
+})
+user.addEventListener('click', () => {
+  secFive.scrollIntoView({behavior:"smooth", block:"center"});
+})
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+})
